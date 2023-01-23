f9d70163a67f8ac3b6b647549a15ccee2e1f1eb0
Maxence <maxence.califano@gmail.com>
2023-01-23 17:49:34 +0200

    build header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,14 +4,15 @@ import styles from '../styles/header.module.css'
 function Header() {
     return (
         <header className={styles.header}>
-            <nav>
-                <span><img src={Logo} alt="logo de SportSee" />SportSee</span>
-                <a href="/">Accueil</a>
-                <a href="/">Profil</a>
-                <a href="/">Réglage</a>
-                <a href="/">Communauté</a>
-            </nav>
+            <span className={styles.logo}>
+                <img src={Logo} alt="logo de SportSee" />
+                <span className={styles.sportsee}>SportSee</span>
+            </span>
 
+            <a href="/">Accueil</a>
+            <a href="/">Profil</a>
+            <a href="/">Réglage</a>
+            <a href="/">Communauté</a>
 
         </header>
     );
