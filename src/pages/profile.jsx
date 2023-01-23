adba7d3ec83deae5cde7757de8edaae5657752ff
Maxence <maxence.califano@gmail.com>
2023-01-23 21:56:10 +0200

    build: greetings

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,37 @@
 import Header from "../Components/Header";
 import LeftNav from "../Components/LeftNav";
+
+import styles from '../styles/profile.module.css';
+
+import { useEffect, useState } from "react";
+
 function Profile() {
+
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        fetch("http://localhost:3000/user/18")
+            .then(response => response.json())
+            .then(res => setUser(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    console.log(user)
+
     return (
         <>
             <Header />
-            <LeftNav />
+            <main>
+                <LeftNav />
+                {user ?
+                    <section className={styles.content}>
+                        <p className={styles.greetings}>Bonjour <span>{user.userInfos.firstName}</span></p>
+                        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                    </section>
+                    : <>{/* TODO METTRE UN LOADER*/}</>
+                }
+
+            </main>
         </>
     );
 }
